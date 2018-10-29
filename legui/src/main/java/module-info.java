e09705d7af9bb20c694cf1edf39fe86e9393cc7f
shchalexander <shchalexander@gmail.com>
2018-10-29 15:27:17 +0300

    fixed module-info descriptor for legui module

--- legui/src/main/java/module-info.java
+++ legui/src/main/java/module-info.java
@@ -1,5 +1,5 @@
 open module org.liquidengine.legui {
     requires transitive org.liquidengine.legui.core;
-    requires org.liquidengine.legui.backend.core;
+    requires org.liquidengine.legui.backend;
     requires org.liquidengine.legui.backend.opengl32;
 }
