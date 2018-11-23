7f695fa9d4b7a27dbf0c3b43573dee86c31c3983
minetoblend <minetoblend@gmail.com>
2018-11-23 18:51:29 +0000

    Deleted unused GL30 and Vulkan source folders

--- backend.vulkan/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by ShchAlexander on 09.08.2018.
- */
-module com.spinyowl.spinygui.backend.vulkan {
-    requires com.spinyowl.spinygui.backend;
-    requires com.spinyowl.spinygui.core;
-    exports com.spinyowl.spinygui.backend.vulkan;
-}
