6b06a645050cc76a716337eebabf1d5a7ca77150
ShchAlexander <shchalexander@gmail.com>
2020-02-03 01:11:58 +0300

    removed unnecessary files for now

--- backend.core/src/main/java/com/spinyowl/spinygui/backend/core/event/handler/SystemEventHandler.java
+++
@@ -1,8 +0,0 @@
-package com.spinyowl.spinygui.backend.core.event.handler;
-
-import com.spinyowl.spinygui.backend.core.event.SystemEvent;
-
-public interface SystemEventHandler<E extends SystemEvent> {
-
-    void handle(E event);
-}
--- backend.glfwutil/src/main/java/com/spinyowl/spinygui/backend/glfwutil/callback/chain/IChainErrorCallback.java
+++
@@ -1,9 +0,0 @@
-package com.spinyowl.spinygui.backend.glfwutil.callback.chain;
-import org.lwjgl.glfw.GLFW;
-import org.lwjgl.glfw.GLFWErrorCallbackI;
-
-/**
- * Instances of this interface may be passed to the {@link GLFW#glfwSetErrorCallback SetErrorCallback} method.
- */
-public interface IChainErrorCallback extends IChainCallback<GLFWErrorCallbackI>, GLFWErrorCallbackI {
-}
