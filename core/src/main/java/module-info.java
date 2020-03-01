30b50370d95c455fb5727274daa91e25173c0c52
ShchAlexander <shchalexander@gmail.com>
2020-03-01 22:10:50 +0300

    fixed build

--- core/src/main/java/module-info.java
+++ core/src/main/java/module-info.java
@@ -11,7 +11,8 @@ open module com.spinyowl.spinygui.core {
     requires org.lwjgl.yoga;
     requires org.lwjgl.yoga.natives;
 
-    requires com.google.common;
+    requires guava.base.r03;
+    requires guava.collections.r03;
     requires io.github.classgraph;
     requires org.antlr.antlr4.runtime;
 
