00fc3956316afd6772c5465ab6b458b93b267f36
Alexander Shcherbin <shchalexander@gmail.com>
2020-04-02 01:02:37 +0300

    added font style and weight css properties handlers

---
+++ core/src/main/java/com/spinyowl/spinygui/core/converter/css/property/font/FontWeightProperty.java
@@ -0,0 +1,16 @@
+package com.spinyowl.spinygui.core.converter.css.property.font;
+
+import static com.spinyowl.spinygui.core.converter.css.Properties.FONT_WEIGHT;
+
+import com.spinyowl.spinygui.core.converter.css.Property;
+import com.spinyowl.spinygui.core.font.FontWeight;
+import com.spinyowl.spinygui.core.style.NodeStyle;
+
+public class FontWeightProperty extends Property<FontWeight> {
+
+    public FontWeightProperty() {
+        super(FONT_WEIGHT, "normal", INHERITED, ANIMATABLE, NodeStyle::setFontWeight,
+            NodeStyle::getFontWeight, FontWeight::find, FontWeight::contains);
+    }
+
+}
--- core/src/main/java/com/spinyowl/spinygui/core/font/FontStyle.java
+++ core/src/main/java/com/spinyowl/spinygui/core/font/FontStyle.java
@@ -1,9 +1,113 @@
 package com.spinyowl.spinygui.core.font;
 
-public enum FontStyle {
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+import java.util.StringJoiner;
+import java.util.concurrent.ConcurrentHashMap;
 
-    NORMAL,
-    ITALIC,
-    OBLIQUE
+public final class FontStyle {
 
+    private static final Map<String, FontStyle> VALUES = new ConcurrentHashMap<>();
+
+    public static final FontStyle NORMAL = FontStyle.create("normal");
+    public static final FontStyle ITALIC = FontStyle.create("italic");
+    public static final FontStyle OBLIQUE = FontStyle.create("oblique");
+
+
+    /**
+     * Name of font style type (should be same as in css specification)
+     */
+    private final String name;
+
+    /**
+     * Creates font style element with specified name.
+     *
+     * @param name name of font style type (should be same as in css specification)
+     */
+    private FontStyle(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Used to find font style element with specified name. Note that name will be converted to
+     * lower case and it should be the same as names of css font style property in css
+     * specification.
+     *
+     * @param name name of font style element.
+     * @return existing font style element or null.
+     */
+    public static FontStyle find(String name) {
+        Objects.requireNonNull(name);
+        return VALUES.get(name.toLowerCase());
+    }
+
+    /**
+     * Used to create new font style element with specified name. Note that name will be converted
+     * to lower case and it should be the same as names of css font style property in css
+     * specification.
+     *
+     * @param name name of font style element.
+     * @return new font style element (or existing one).
+     */
+    public static FontStyle create(String name) {
+        Objects.requireNonNull(name);
+        return VALUES.computeIfAbsent(name.toLowerCase(), FontStyle::new);
+    }
+
+    /**
+     * Returns set of all available font style values.
+     *
+     * @return set of all available font style values.
+     */
+    public static Set<FontStyle> values() {
+        return Set.copyOf(VALUES.values());
+    }
+
+    /**
+     * Returns true there is a font style value wth specified name.
+     *
+     * @param name font style name.
+     * @return true there is a font style value wth specified name.
+     */
+    public static boolean contains(String name) {
+        if (name == null) {
+            return false;
+        }
+        return values().stream().map(FontStyle::getName)
+            .anyMatch(v -> v.equalsIgnoreCase(name));
+    }
+
+    /**
+     * Name of font style.
+     *
+     * @return name of font style.
+     */
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        FontStyle fontStyle = (FontStyle) o;
+        return Objects.equals(name, fontStyle.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+
+    @Override
+    public String toString() {
+        return new StringJoiner(", ", FontStyle.class.getSimpleName() + "[", "]")
+            .add("name='" + name + "'")
+            .toString();
+    }
 }
