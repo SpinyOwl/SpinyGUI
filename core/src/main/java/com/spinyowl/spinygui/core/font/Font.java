45ae65f6c31fb428abc3df02230b576019afffd5
SpinyOwl <shchalexander@gmail.com>
2021-11-21 12:57:14 +0100

    Updated font structure that holds registered fonts.

--- core/src/main/java/com/spinyowl/spinygui/core/font/Font.java
+++ core/src/main/java/com/spinyowl/spinygui/core/font/Font.java
@@ -1,11 +1,9 @@
 package com.spinyowl.spinygui.core.font;
 
 import java.util.HashMap;
-import java.util.LinkedHashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Objects;
 import lombok.Data;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
@@ -16,38 +14,32 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class Font {
 
-  private static final Map<String, Map<FontKey, Font>> fontFamilies = new HashMap<>();
-  private static Map<FontKey, Font> fonts = new HashMap<>();
+  private static final Map<String, List<Font>> fontFamilies = new HashMap<>();
 
-  @NonNull private final String name;
-
+  @NonNull private final String fontFamily;
   @NonNull private final FontStyle style;
-
   @NonNull private final FontStretch width;
-
   @NonNull private final FontWeight weight;
 
   @NonNull private final String path;
 
-  public Font(String name, String path) {
-    this(name, FontStyle.NORMAL, FontStretch.NORMAL, FontWeight.NORMAL, path);
+  public Font(@NonNull String fontFamily, @NonNull String path) {
+    this(fontFamily, FontStyle.NORMAL, FontStretch.NORMAL, FontWeight.NORMAL, path);
   }
 
-  public Font(String name, FontStyle style, String path) {
-    this(name, style, FontStretch.NORMAL, FontWeight.NORMAL, path);
+  public Font(@NonNull String fontFamily, @NonNull FontStyle style, @NonNull String path) {
+    this(fontFamily, style, FontStretch.NORMAL, FontWeight.NORMAL, path);
   }
 
-  public static void addFont(Font font) {
-    Objects.requireNonNull(font);
-
-    var key = new FontKey(font.name(), font.style(), font.width(), font.weight());
-
-    Map<FontKey, Font> fonts =
-        fontFamilies.computeIfAbsent(font.name(), n -> new LinkedHashMap<>());
-    if (fonts.containsKey(key) && log.isWarnEnabled()) {
-      log.warn("Font '{}' will be replaced.", font.name());
+  public static void addFont(@NonNull Font font) {
+    List<Font> fonts = fontFamilies.computeIfAbsent(font.fontFamily(), n -> new LinkedList<>());
+    if (hasFont(font.fontFamily, font.style, font.weight, font.width)) {
+      if (log.isWarnEnabled()) {
+        log.warn("Font '{}' will be replaced.", font.fontFamily());
+      }
+      fonts.removeAll(getFonts(font.fontFamily, font.style, font.weight, font.width));
     }
-    fonts.put(key, font);
+    fonts.add(font);
   }
 
   /**
@@ -78,6 +70,18 @@ public class Font {
    *
    * @param name font family name.
    * @param style font style
+   * @return obtained font.
+   */
+  public static List<Font> getFonts(String name, FontStyle style) {
+    return getFonts(name, style, null, null);
+  }
+
+  /**
+   * Search for fonts with specified parameters. Any parameter could be nullable. In this case this
+   * parameter will not be used during search.
+   *
+   * @param name font family name.
+   * @param style font style
    * @param weight font weight
    * @return obtained font.
    */
@@ -91,10 +95,14 @@ public class Font {
    *
    * @param name font family name.
    * @param style font style
+   * @param width font width
+   * @param weight font weight
    * @return obtained font.
    */
-  public static List<Font> getFonts(String name, FontStyle style) {
-    return getFonts(name, style, null, null);
+  public static List<Font> getFonts(
+      String name, FontStyle style, FontWeight weight, FontStretch width) {
+    List<Font> fonts = fontFamilies.get(name);
+    return fonts.stream().filter(font -> checkFont(font, name, style, weight, width)).toList();
   }
 
   /**
@@ -142,25 +150,6 @@ public class Font {
   }
 
   /**
-   * Search for fonts with specified parameters. Any parameter could be nullable. In this case this
-   * parameter will not be used during search.
-   *
-   * @param name font family name.
-   * @param style font style
-   * @param width font width
-   * @param weight font weight
-   * @return obtained font.
-   */
-  public static List<Font> getFonts(
-      String name, FontStyle style, FontWeight weight, FontStretch width) {
-    Map<FontKey, Font> fonts = fontFamilies.get(name);
-    return fonts.entrySet().stream()
-        .filter(pair -> checkFont(pair.getKey(), name, style, weight, width))
-        .map(Entry::getValue)
-        .toList();
-  }
-
-  /**
    * Returns true if there is any font with specified parameters. Any parameter could be nullable.
    * In this case this parameter will not be used during search.
    *
@@ -173,23 +162,14 @@ public class Font {
   public static boolean hasFont(
       String name, FontStyle style, FontWeight weight, FontStretch width) {
     return fontFamilies.containsKey(name)
-        && fontFamilies.get(name).keySet().stream()
-            .anyMatch(k -> checkFont(k, name, style, weight, width));
+        && fontFamilies.get(name).stream().anyMatch(k -> checkFont(k, name, style, weight, width));
   }
 
   private static boolean checkFont(
-      FontKey fontKey, String name, FontStyle style, FontWeight weight, FontStretch width) {
-    return (name == null || name.equalsIgnoreCase(fontKey.name))
-        && (style == null || style.equals(fontKey.style))
-        && (weight == null || weight.equals(fontKey.weight))
-        && (width == null || width.equals(fontKey.width));
-  }
-
-  @Data
-  private static class FontKey {
-    @NonNull private final String name;
-    @NonNull private final FontStyle style;
-    @NonNull private final FontStretch width;
-    @NonNull private final FontWeight weight;
+      Font font, String name, FontStyle style, FontWeight weight, FontStretch width) {
+    return (name == null || name.equalsIgnoreCase(font.fontFamily))
+        && (style == null || style.equals(font.style))
+        && (weight == null || weight.equals(font.weight))
+        && (width == null || width.equals(font.width));
   }
 }
