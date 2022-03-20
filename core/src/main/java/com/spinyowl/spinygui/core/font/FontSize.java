83f5e1f46d70153e109ded3df15f91b55b61c74d
SpinyOwl <shchalexander@gmail.com>
2022-03-20 04:15:03 +0100

    Updated font size class to use default font size as medium and size modifiers for other keywords.

--- core/src/main/java/com/spinyowl/spinygui/core/font/FontSize.java
+++ core/src/main/java/com/spinyowl/spinygui/core/font/FontSize.java
@@ -9,39 +9,46 @@ import lombok.Getter;
 import lombok.NonNull;
 import lombok.ToString;
 
-@Getter
 @EqualsAndHashCode
 @ToString
 public class FontSize {
 
-  /**
-   * Stores all existing dictionary values.
-   */
+  /** Stores all existing dictionary values. */
   private static final Map<String, FontSize> VALUES = new ConcurrentHashMap<>();
 
-  public static final FontSize XX_LARGE = FontSize.create("xx-large", 22);
-  public static final FontSize X_LARGE = FontSize.create("x-large", 18);
-  public static final FontSize LARGE = FontSize.create("large", 16);
-  public static final FontSize MEDIUM = FontSize.create("medium", 14);
-  public static final FontSize SMALL = FontSize.create("small", 12);
-  public static final FontSize X_SMALL = FontSize.create("x-small", 10);
-  public static final FontSize XX_SMALL = FontSize.create("xx-small", 9);
+  public static final FontSize XX_LARGE = FontSize.create("xx-large", 6);
+  public static final FontSize X_LARGE = FontSize.create("x-large", 4);
+  public static final FontSize LARGE = FontSize.create("large", 2);
+  public static final FontSize MEDIUM = FontSize.create("medium", 0);
+  public static final FontSize SMALL = FontSize.create("small", 2);
+  public static final FontSize X_SMALL = FontSize.create("x-small", 4);
+  public static final FontSize XX_SMALL = FontSize.create("xx-small", 6);
 
-  /**
-   * Name of FontSize element.
-   */
-  private final String name;
-  private final int size;
+  // TODO: Move this default font size to settings.
+  private static int defaultFontSize = 16;
+
+  /** Name of FontSize element. */
+  @Getter private final String name;
+
+  private final int sizeModifier;
 
   /**
    * Creates FontSize element with specified name.
    *
    * @param name name of FontSize type.
-   * @param size font size in pixels.
+   * @param sizeModifier font size in pixels.
    */
-  private FontSize(@NonNull String name, int size) {
+  private FontSize(@NonNull String name, int sizeModifier) {
     this.name = name;
-    this.size = size;
+    this.sizeModifier = sizeModifier;
+  }
+
+  public static int defaultFontSize() {
+    return defaultFontSize;
+  }
+
+  public static void defaultFontSize(int defaultFontSize) {
+    FontSize.defaultFontSize = defaultFontSize;
   }
 
   /**
@@ -53,8 +60,8 @@ public class FontSize {
    * @return new FontSize element (or existing one).
    */
   public static FontSize create(@NonNull String name, int size) {
-    return VALUES.computeIfAbsent(Objects.requireNonNull(name).toLowerCase(),
-        name1 -> new FontSize(name1, size));
+    return VALUES.computeIfAbsent(
+        Objects.requireNonNull(name).toLowerCase(), name1 -> new FontSize(name1, size));
   }
 
   /**
@@ -87,4 +94,7 @@ public class FontSize {
     return VALUES.containsKey(name.toLowerCase());
   }
 
+  public int size() {
+    return defaultFontSize() + sizeModifier;
+  }
 }
