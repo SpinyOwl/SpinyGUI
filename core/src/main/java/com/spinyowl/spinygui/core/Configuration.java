56a695c746bf69d4eb70ab421fc406d258000ba9
SpinyOwl <shchalexander@gmail.com>
2022-09-02 03:04:38 +0200

    (WIP) Update Configuration class to support default values.

--- core/src/main/java/com/spinyowl/spinygui/core/Configuration.java
+++ core/src/main/java/com/spinyowl/spinygui/core/Configuration.java
@@ -1,10 +1,20 @@
 package com.spinyowl.spinygui.core;
 
+import java.util.function.Function;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
 /**
  * Configuration class that used to define configuration for GUI system.
  *
  * @param <T> type of configuration value.
  */
+@Getter
+@Setter
+@EqualsAndHashCode
+@ToString
 public final class Configuration<T> {
 
   public static final Configuration<Boolean> INITIALIZE_SERVICES =
@@ -21,55 +31,37 @@ public final class Configuration<T> {
   public static final Configuration<String> TIME_SERVICE =
       new Configuration<>("spinygui.service.time", Initializer.STRING);
 
+  public static final Configuration<Float> DEFAULT_LINE_HEIGHT =
+      new Configuration<>("spinygui.font.lineHeight.default", Initializer.FLOAT, 1.2F);
+
   private final String name;
+  private final T defaultValue;
   private T value;
 
   private Configuration(String name, Initializer<T> initializer) {
+    this(name, initializer, null);
+  }
+
+  private Configuration(String name, Initializer<T> initializer, T defaultValue) {
     this.name = name;
-    this.value = initializer.init(name);
-  }
-
-  /**
-   * Returns configuration name.
-   *
-   * @return configuration name.
-   */
-  public String getName() {
-    return name;
-  }
-
-  /**
-   * Returns configuration value.
-   *
-   * @return configuration value
-   */
-  public T getValue() {
-    return value;
-  }
-
-  public void setValue(T value) {
-    this.value = value;
-  }
-
-  public T getState(T defaultValue) {
-    var state = this.value;
-    if (state == null) {
-      state = defaultValue;
-    }
-
-    return state;
+    this.defaultValue = defaultValue;
+    this.value = initializer.initialize(name, defaultValue);
   }
 
   protected interface Initializer<T> {
 
-    Initializer<Boolean> BOOLEAN =
-        configuration -> {
-          String value = System.getProperty(configuration);
-          return value == null ? null : Boolean.parseBoolean(value);
-        };
-    Initializer<Integer> INT = Integer::getInteger;
-    Initializer<String> STRING = System::getProperty;
+    Initializer<Boolean> BOOLEAN = fromSystemProperties(Boolean::parseBoolean);
+    Initializer<Integer> INT = fromSystemProperties(Integer::parseInt);
+    Initializer<String> STRING = fromSystemProperties(Function.identity());
+    Initializer<Float> FLOAT = fromSystemProperties(Float::parseFloat);
 
-    T init(String configuration);
+    static <T> Initializer<T> fromSystemProperties(Function<String, T> convert) {
+      return (propert, defaultValue) -> {
+        String value = System.getProperty(propert);
+        return value == null ? defaultValue : convert.apply(value);
+      };
+    }
+
+    T initialize(String configuration, T defaultValue);
   }
 }
