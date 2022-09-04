20a05eee77519bc6b7c15a998fea7e58393320b8
SpinyOwl <shchalexander@gmail.com>
2022-09-04 23:06:11 +0200

    (WIP) Add TermFunction so it would be possible to pass function to stylesheet.

---
+++ core/src/main/java/com/spinyowl/spinygui/core/style/stylesheet/term/TermFunction.java
@@ -0,0 +1,21 @@
+package com.spinyowl.spinygui.core.style.stylesheet.term;
+
+import com.spinyowl.spinygui.core.style.stylesheet.Term;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+
+@Getter
+@EqualsAndHashCode
+public class TermFunction extends TermList {
+  private final String name;
+
+  public TermFunction(String name, Operator operator, Term<?>... value) {
+    super(operator, value);
+    this.name = name;
+  }
+
+  @Override
+  public String toString() {
+    return name + "(" + super.toString() + ")";
+  }
+}
