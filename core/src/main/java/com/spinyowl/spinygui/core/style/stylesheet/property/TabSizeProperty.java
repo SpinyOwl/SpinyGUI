9461e75a95484785aaf94fc1d2c17c1afbaca57b
SpinyOwl <shchalexander@gmail.com>
2022-09-05 03:03:00 +0200

    (WIP) Update `tab-size` property.

--- core/src/main/java/com/spinyowl/spinygui/core/style/stylesheet/property/TabSizeProperty.java
+++ core/src/main/java/com/spinyowl/spinygui/core/style/stylesheet/property/TabSizeProperty.java
@@ -3,13 +3,9 @@ package com.spinyowl.spinygui.core.style.stylesheet.property;
 import static com.spinyowl.spinygui.core.style.stylesheet.Properties.TAB_SIZE;
 
 import com.spinyowl.spinygui.core.style.stylesheet.Property;
-import com.spinyowl.spinygui.core.style.stylesheet.extractor.ValueExtractor;
-import com.spinyowl.spinygui.core.style.stylesheet.extractor.ValueExtractors;
 import com.spinyowl.spinygui.core.style.stylesheet.term.TermInteger;
 
 public class TabSizeProperty extends Property {
-
-  private static final ValueExtractor<Integer> extractor = ValueExtractors.of(Integer.class);
 
   public TabSizeProperty() {
     super(
