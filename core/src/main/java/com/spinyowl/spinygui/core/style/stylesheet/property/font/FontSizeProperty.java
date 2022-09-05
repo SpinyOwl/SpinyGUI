6eb5a09f3a6a142cc45692eac06458d3838f9006
SpinyOwl <shchalexander@gmail.com>
2022-09-05 03:02:05 +0200

    (WIP) Update `font-size` property.

--- core/src/main/java/com/spinyowl/spinygui/core/style/stylesheet/property/font/FontSizeProperty.java
+++ core/src/main/java/com/spinyowl/spinygui/core/style/stylesheet/property/font/FontSizeProperty.java
@@ -1,35 +1,20 @@
 package com.spinyowl.spinygui.core.style.stylesheet.property.font;
 
 import static com.spinyowl.spinygui.core.style.stylesheet.Properties.FONT_SIZE;
+
 import com.spinyowl.spinygui.core.font.FontSize;
 import com.spinyowl.spinygui.core.style.stylesheet.Property;
-import com.spinyowl.spinygui.core.style.stylesheet.extractor.ValueExtractor;
-import com.spinyowl.spinygui.core.style.stylesheet.extractor.ValueExtractors;
-import com.spinyowl.spinygui.core.style.types.length.Length;
+import com.spinyowl.spinygui.core.style.stylesheet.term.TermIdent;
+import com.spinyowl.spinygui.core.style.stylesheet.term.TermLength;
 
 public class FontSizeProperty extends Property {
-  @SuppressWarnings("rawtypes")
-  private static final ValueExtractor<Length> extractor = ValueExtractors.of(Length.class);
-
   public FontSizeProperty() {
     super(
         FONT_SIZE,
-        FontSize.MEDIUM.name(),
+        new TermIdent(FontSize.MEDIUM.name()),
         INHERITABLE,
         ANIMATABLE,
-        (value, styles) -> styles.put(FONT_SIZE, extract(value)),
-        FontSizeProperty::test);
-  }
-
-  @SuppressWarnings("rawtypes")
-  public static Length extract(String value) {
-    if (FontSize.contains(value)) {
-      return Length.pixel(FontSize.find(value).size());
-    }
-    return extractor.extract(value);
-  }
-
-  public static boolean test(String value) {
-    return FontSize.contains(value) || extractor.isValid(value);
+        put(FONT_SIZE, TermIdent.class, FontSize::find).or(put(FONT_SIZE, TermLength.class)),
+        check(TermIdent.class, FontSize::contains).or(TermLength.class::isInstance));
   }
 }
